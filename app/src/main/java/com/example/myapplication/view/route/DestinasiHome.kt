deaafbe725f7fce1c70fcdf220dda218d01ac7d6
dianfitrip <dianfitri683@gmail.com>
2025-11-28 13:57:07 +0700

    tambahkan DestinasiHome sebagai tujuan navigasi dengan route 'home' dan titleRes app_name

---
+++ app/src/main/java/com/example/myapplication/view/route/DestinasiHome.kt
@@ -0,0 +1,9 @@
+package com.example.myapplication.view.route
+
+import com.example.myapplication.R
+import com.example.myapplication.view.route.DestinasiNavigasi
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = R.string.app_name
+}
