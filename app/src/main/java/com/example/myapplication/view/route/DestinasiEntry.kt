cd3820fa4e4788986b0bdc6a23784a30604d2247
dianfitrip <dianfitri683@gmail.com>
2025-11-28 13:58:25 +0700

    tambahkan DestinasiEntry sebagai tujuan input siswa dengan route item_entry dan titleRes entry_siswa

---
+++ app/src/main/java/com/example/myapplication/view/route/DestinasiEntry.kt
@@ -0,0 +1,9 @@
+package com.example.myapplication.view.route
+
+import com.example.myapplication.R
+import com.example.myapplication.view.route.DestinasiNavigasi
+
+object DestinasiEntry : DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = R.string.entry_siswa
+}
