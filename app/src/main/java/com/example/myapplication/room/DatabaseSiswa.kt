be1a639f277381bebb4a38f95c2e3e3942b3571f
dianfitrip <dianfitri683@gmail.com>
2025-11-28 14:03:16 +0700

    tambahkan singleton DatabaseSiswa dengan Room database builder

--- app/src/main/java/com/example/myapplication/room/DatabaseSiswa.kt
+++ app/src/main/java/com/example/myapplication/room/DatabaseSiswa.kt
@@ -10,5 +10,20 @@ abstract class DatabaseSiswa : RoomDatabase() {
 
     abstract fun siswaDao(): SiswaDao
 
+    companion object {
+        @Volatile
+        private var Instance: DatabaseSiswa? = null
 
+        fun getDatabase(context: Context): DatabaseSiswa {
+            return Instance ?: synchronized(this) {
+                Room.databaseBuilder(
+                    context,
+                    DatabaseSiswa::class.java,
+                    "siswa_database"
+                )
+                    .build()
+                    .also { Instance = it }
+            }
+        }
+    }
 }
