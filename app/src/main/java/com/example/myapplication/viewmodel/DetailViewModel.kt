f671cc19684389f4e784fca18f8f59d26c766a5b
dianfitrip <dianfitri683@gmail.com>
2025-12-05 09:55:38 +0700

    menambahkan konstanta TIMEOUT_MILLIS pada DetailViewModel

--- app/src/main/java/com/example/myapplication/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/myapplication/viewmodel/DetailViewModel.kt
@@ -24,6 +24,10 @@ class DetailViewModel(
         repositoriSiswa.deleteSiswa(uiDetailState.value.detailSiswa.toSiswa())
     }
 
+    companion object {
+        private const val TIMEOUT_MILLIS = 5_000L
+    }
+
 
 }
 
