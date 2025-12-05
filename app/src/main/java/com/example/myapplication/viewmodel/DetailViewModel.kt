916a7540570396d0de810511e1bef80685a2d4db
dianfitrip <dianfitri683@gmail.com>
2025-12-05 09:52:47 +0700

    menambahkan DetailViewModel yang menerima SavedStateHandle dan RepositoriSiswa

---
+++ app/src/main/java/com/example/myapplication/viewmodel/DetailViewModel.kt
@@ -0,0 +1,12 @@
+package com.example.myapplication.viewmodel
+
+
+
+class DetailViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val repositoriSiswa: RepositoriSiswa
+) : ViewModel() {
+
+
+}
+
